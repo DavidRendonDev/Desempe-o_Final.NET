daffc4291cb459bf76db3c8a0c2cc2b486946327
DavidRendonDev <davidrex114@hotmail.com>
2025-12-09 06:58:30 -0500

    first commit

--- TalentoP.Api/Program.cs
+++ TalentoP.Api/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openAPI
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
